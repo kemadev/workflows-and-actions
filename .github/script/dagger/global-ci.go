97ee68a558c93149320d5e2e0d141fa48497fde3
kema-dev <github@kema.dev>
2025-04-16 11:21:07 +0200

    up

--- .github/script/dagger/global-ci.go
+++ .github/script/dagger/global-ci.go
@@ -6,7 +6,7 @@ import (
 	"dagger/dagger/internal/dagger"
 )
 
-func (m *Dagger) GlobalCi(
+func (m *Dagger) SemgrepCi(
 	ctx context.Context,
 	// +defaultPath="/"
 	source *dagger.Directory,
@@ -16,10 +16,43 @@ func (m *Dagger) GlobalCi(
 		WithMountedDirectory("/src", source).
 		WithWorkdir("/src").
 		// Local
-		WithExec([]string{"sh", "-c", "semgrep scan --metrics=off --error --config 'p/default' --config 'p/gitleaks' --config 'p/kubernetes' --config 'p/dockerfile'"}).
+		WithExec([]string{"sh", "-c", "semgrep scan --metrics=off --error --config 'p/default' --config 'p/kubernetes' --config 'p/dockerfile'"}).
 		// PR
 		// WithExec([]string{"sh", "-c", "trufflehog --fail --no-update --github-actions--no-verification  git file://. --since-commit main"}).
 		// TODO cron
 		// WithExec([]string{"sh", "-c", "trufflehog --fail --no-update --github-actions--no-verification  git file://."}).
 		Stdout(ctx)
+}
+
+func (m *Dagger) GitleaksCi(
+	ctx context.Context,
+	// +defaultPath="/"
+	source *dagger.Directory,
+) (string, error) {
+	return dag.Container().
+		From("ghcr.io/gitleaks/gitleaks:latest").
+		WithMountedDirectory("/src", source).
+		WithWorkdir("/src").
+		// Local
+		WithExec([]string{"sh", "-c", "gitleaks git --verbose"}).
+		// PR
+		// WithExec([]string{"sh", "-c", "trufflehog --fail --no-update --github-actions--no-verification  git file://. --since-commit main"}).
+		// TODO cron
+		// WithExec([]string{"sh", "-c", "trufflehog --fail --no-update --github-actions--no-verification  git file://."}).
+		Stdout(ctx)
+}
+
+func (m *Dagger) GlobalCi(
+	ctx context.Context,
+	// +defaultPath="/"
+	source *dagger.Directory,
+) (string, error) {
+	s1, err1 := m.SemgrepCi(ctx, source)
+	s2, err2 := m.GitleaksCi(ctx, source)
+	fs := s1 + s2
+	ferr := err1.Error() + err2.Error()
+	if ferr != "" {
+		return fs, err1
+	}
+	return fs, nil
 }
