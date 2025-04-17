fa9dd2252f1c5e0487b1eaf2340bbfb13b9bbedc
kema-dev <github@kema.dev>
2025-04-17 14:29:35 +0200

    up

--- pkg/internal/sarif-parser/main.go
+++ pkg/internal/sarif-parser/main.go
@@ -9,12 +9,16 @@ import (
 )
 
 var (
+	// Set in workflow-base image
 	path string = os.Getenv("KEMA_CI_SARIF_REPORT_PATH")
-	gha  bool
-	rc   int
+	// Running in GHA context?
+	gha bool
+	// Exit code to use
+	rc int
 )
 
 func init() {
+	// Set when running in GHA context, see https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
 	if os.Getenv("GITHUB_ACTIONS") == "true" {
 		gha = true
 	}
